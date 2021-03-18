584523ed5ad99515b2b433f9d66710148823da53
GRHSXY <1399784328@qq.com>
2021-03-18 16:13:12 +0800

    完成./domain/中其余4个类的代码

---
+++ domain/UserHoldings.py
@@ -0,0 +1,12 @@
+class User_holdings:
+    def __init__(self, **kwargs):
+        self.user_id = kwargs['user_id']
+        self.stock_index = kwargs['stock_index']
+        self.stock_amount = kwargs['stock_amount']
+        self.bought_price = kwargs['bought_price']
+        self.bought_total_price = kwargs['bought_total_price']
+
+
+if __name__ == "__main__":
+    user_holdings = User_holdings(user_id=114514, stock_index=110260,
+                                        stock_amount=5, bought_price=50, bought_total_price=500)
