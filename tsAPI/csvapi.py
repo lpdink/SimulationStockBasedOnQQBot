269ff5e409499e7ece37466299d512f7df5d2db3
A-Salty-Fish <1309032417@qq.com>
2021-03-21 17:51:23 +0800

    按名字搜索股票代码

--- tsAPI/csvapi.py
+++ tsAPI/csvapi.py
@@ -1,6 +1,7 @@
 import csv
 from datetime import date
 from fuzzywuzzy import fuzz
+# -*- coding:utf-8 -*-
 
 dic = {}
 filename = "../csv/"+date.today().__str__()+".csv"
@@ -17,8 +18,16 @@ def initDic():
             for val in firstRow:
                 dic[val]=index
                 index+=1
-def getIdByName(name):
+            for key in dic:
+                print("\n{0}:{1}".format(key,dic[key]))
+def getCodeByName(name):
     with open(filename,'r' , encoding='utf-8') as f:
         reader = csv.reader(f)
         rows = [row for row in reader]
-        print(rows[1])
+        codeIndex = dic["code"]
+        nameIndex = dic["name"]
+        codes = []
+        for row in rows:
+            if row[nameIndex] == name:
+                codes.append([row[nameIndex],row[codeIndex]])
+        print(codes)
