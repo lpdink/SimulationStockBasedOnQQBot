904c11aca59b79a0a7d6f550fa74ea78faa0e225
A-Salty-Fish <1309032417@qq.com>
2021-03-21 14:46:27 +0800

    初始化单元测试

---
+++ unitTest/apiTest.py
@@ -0,0 +1,10 @@
+import unittest
+import tushare as ts
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(False, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
